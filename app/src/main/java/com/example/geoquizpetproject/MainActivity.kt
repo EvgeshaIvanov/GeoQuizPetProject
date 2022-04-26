a7614b12a5e3bc4116570921e7016ab50791bcd6
Evgesha <most1360@yandex.ru>
2022-04-26 15:40:43 +0300

    Добавлена навигация

--- app/src/main/java/com/example/geoquizpetproject/MainActivity.kt
+++ app/src/main/java/com/example/geoquizpetproject/MainActivity.kt
@@ -1,16 +1,11 @@
 package com.example.geoquizpetproject
 
-import android.annotation.SuppressLint
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.geoquizpetproject.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,5 +19,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         navController = Navigation.findNavController(this, R.id.nav_graph)
+
     }
 }
